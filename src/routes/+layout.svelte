cbe89e7b8c6ee8df8193744370a811138db22d43
mariuslang <marius@mariuslang.de>
2026-01-02 14:25:10 +0100

    fix routes endpoint

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import "../app.css";
+	import { injectAnalytics } from "@vercel/analytics/sveltekit";
+
+	injectAnalytics({ mode: "production" });
 
 	let { children } = $props();
 </script>
